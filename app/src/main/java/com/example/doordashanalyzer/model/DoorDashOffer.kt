78d9ab036cb9cea4ceefc1c7648ea641e30263a1
woraklzv <woraklzv@gmail.com>
2025-09-23 21:57:42 -0500

    Delete app/src/main directory

--- app/src/main/java/com/example/doordashanalyzer/model/DoorDashOffer.kt
+++
@@ -1,8 +0,0 @@
-package com.example.doordashanalyzer.model
-
-data class DoorDashOffer(
-    val restaurant: String,
-    val amount: Double,
-    val distance: Double,
-    val estimatedTime: Int
-)
